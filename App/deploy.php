9af9c99fdf5616e1d051f0749678e6437ce96605
Carl Newton <carlnewton@gmail.com>
2024-12-22 12:42:17 +0000

    Add database migrations to deploy.php

--- App/deploy.php
+++ App/deploy.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Deployer;
 
 require 'recipe/symfony.php';
@@ -20,20 +21,13 @@ host('example.com')
     ->set('sub_directory', 'App')
 ;
 
-// Hooks
-task('habitat:stop_messenger', function () {
-    if (has('previous_release')) {
-        run('{{bin/php}} {{previous_release}}/bin/console messenger:stop-workers');
-    }
-});
-
 task('habitat:generate_app_secret', function () {
     $secret = trim(shell_exec('openssl rand -hex 16'));
     $envFile = '/var/www/html/shared/.env.local';
 
     $exists = run("grep -q '^APP_SECRET=' $envFile && echo 'exists' || echo 'not_exists'");
 
-    if ($exists === 'exists') {
+    if ('exists' === $exists) {
         run("sed -i 's/^APP_SECRET=.*/APP_SECRET={$secret}/' $envFile");
     } else {
         run("echo 'APP_SECRET={$secret}' >> $envFile");
@@ -41,12 +35,18 @@ task('habitat:generate_app_secret', function () {
 });
 
 task('habitat:generate_assets', function () {
-    run("cd {{release_path}} && npm install --loglevel=verbose && npm run build");
+    run('cd {{release_path}} && npm install --loglevel=verbose && npm run build');
 });
 
-task('habitat:start_messenger', function () {
-    run('{{bin/php}} {{release_path}}/bin/console messenger:consume');
-});
-after('deploy:shared', 'habitat:generate_app_secret');
-after('deploy:writable', 'habitat:generate_assets');
+task('habitat:prepare', [
+    'habitat:generate_app_secret',
+]);
+
+task('habitat:deploy', [
+    'habitat:generate_assets',
+]);
+
+after('deploy:shared', 'habitat:prepare');
+after('deploy:vendors', 'habitat:deploy');
+after('habitat:deploy', 'database:migrate');
 after('deploy:failed', 'deploy:unlock');
