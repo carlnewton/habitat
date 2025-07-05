29af7a31521cb6595a8d7a781a0f98cdec5795be
Carl Newton <carlnewton@gmail.com>
2025-07-05 09:04:04 +0100

    Add profiler and correct auto_mapping setting

--- App/config/bundles.php
+++ App/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
